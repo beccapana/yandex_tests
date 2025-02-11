b73b305c454943cbd3b82eb6e1d87caaad5d9d21
beccapana <88428390+beccapana@users.noreply.github.com>
2025-02-11 23:17:48 +0300

    test-cases updated

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 current_dir = os.path.dirname(__file__)
 DATA_DIR = os.path.join(current_dir, "test_cases")
 
-NEW_PET_POS_FILE = os.path.join(DATA_DIR, "NewPetPos.xlsx")
-NEW_PET_NEG_FILE = os.path.join(DATA_DIR, "NewPetNeg.xlsx")
+NEW_PET_POS_FILE = os.path.join(DATA_DIR, "NewPetPos.xlsx") #pairwise.teremokgames.com/5jl9g/
+NEW_PET_NEG_FILE = os.path.join(DATA_DIR, "NewPetNeg.xlsx") #pairwise.teremokgames.com/5jl6o/
